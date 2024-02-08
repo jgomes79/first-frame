5f6fd344bff0c2c096532484d8eb9fe7be65ab75
jgomes79 <jgomes79@hotmail.es>
2024-02-08 01:23:00 +0100

    frame working

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,120 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { html } from 'hono/html'
-import type { FrameSignaturePacket } from './types'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { html } from 'hono/html';
+import type { FrameSignaturePacket } from './types';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  const frameImage = `https://placehold.co/600x400?text=Hello+World`
-  const framePostUrl = c.req.url
+const data = [{
+  name: 'Moneyland',
+  image: 'https://smartrightsnfts.s3.eu-west-3.amazonaws.com/3a2b3338-6b77-4708-8e0b-2f4b486fa637.jpg',
+  url: 'https://alpha.givitnft.com/collection/moneyland'
+}, {
+  name: 'SuperPioneros',
+  image: 'https://smartrightsnfts.s3.eu-west-3.amazonaws.com/ea4ad5cc-53fb-4994-b048-fbe60a544d2f.png',
+  url: 'https://alpha.givitnft.com/collection/superpioneros'
+}, {
+  name: 'gggggggggggg',
+  image: 'https://smartrightsnfts.s3.eu-west-3.amazonaws.com/e9233941-8566-42b8-988d-d924f060c4c4.jpg',
+  url: 'https://alpha.givitnft.com/collection/gggggggggggg'
+}, {
+  name: 'javierdelahozm',
+  image: 'https://smartrightsnfts.s3.eu-west-3.amazonaws.com/98f9597c-a945-47c9-bb34-2fda2719ac9a.png',
+  url: 'https://alpha.givitnft.com/collection/javierdelahozm'
+}];
 
-  return c.html(html`
+const getInitialHTML = (framePostUrl: string) => {
+  return html`
     <html lang="en">
       <head>
-        <meta property="og:image" content="${frameImage}" />
+        <meta property="og:image" content="https://smartrightsnfts.s3.eu-west-3.amazonaws.com/GivitNFT.png" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${frameImage}" />
         <meta property="fc:frame:post_url" content="${framePostUrl}" />
-        <meta property="fc:frame:button:1" content="Green" />
-        <meta property="fc:frame:button:2" content="Purple" />
-        <meta property="fc:frame:button:3" content="Red" />
-        <meta property="fc:frame:button:4" content="Blue" />
-        <title>Farcaster Frames</title>
+        <meta property="fc:frame:image" content="https://smartrightsnfts.s3.eu-west-3.amazonaws.com/GivitNFT.png" />
+        <meta property="fc:frame:button:1" content="View communities" />
+        <title>GivitNFT Communities</title>
       </head>
       <body>
-        <h1>Hello Farcaster!</h1>
+        <h1>GivitNFT. Visit us in <a href="https://givitnft.com">https://givitnft.com</a></h1>
       </body>
     </html>
-  `)
-})
+  `;
+}
 
-app.post('/', async (c) => {
+app.get('/', (c) => {
+  const framePostUrl = c.req.url.replace('http://', 'https://') + '0';
+  return c.html(getInitialHTML(framePostUrl));
+});
+
+app.post('/:id', async (c) => {
   try {
-    const body = await c.req.json<FrameSignaturePacket>()
-    const { buttonIndex, inputText } = body.untrustedData
+    const id = Number(c.req.param('id'));
 
-    const backgroundColors = ['green', 'purple', 'red', 'blue']
+    if (id >= data.length) {
+      const framePostUrl = c.req.url.replace('http://', 'https://').slice(0, c.req.url.lastIndexOf('/') + 1) + "/0";
+      return c.html(getInitialHTML(framePostUrl));
+    }
 
-    const imageText = encodeURIComponent(inputText || 'Hello World')
-    const imageColor = backgroundColors[buttonIndex - 1] || 'white'
+    const dataId = id + 1;
+    console.log('dataId', dataId);
 
-    const frameImage = `https://placehold.co/600x400/${imageColor}/white?text=${imageText}`
-    const framePostUrl = c.req.url
+    const framePostUrl = c.req.url.replace('http://', 'https://').slice(0, c.req.url.lastIndexOf('/') + 1) + "/" + dataId.toString();
+    console.log(framePostUrl);
 
     return c.html(html`
       <html lang="en">
         <head>
+          <meta property="og:image" content="${data[dataId-1].image}" />
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="${frameImage}" />
           <meta property="fc:frame:post_url" content="${framePostUrl}" />
-          <meta property="fc:frame:input:text" content="Enter a message" />
-          <meta property="fc:frame:button:1" content="Green" />
-          <meta property="fc:frame:button:2" content="Purple" />
-          <meta property="fc:frame:button:3" content="Red" />
-          <meta property="fc:frame:button:4" content="Blue" />
-          <title>Farcaster Frames</title>
+          <meta property="fc:frame:image" content="${data[dataId-1].image}" />
+          <meta property="fc:frame:button:1" content="Go to ${data[dataId-1].name} page" />
+          <meta property="fc:frame:button:1:action" content="link" />
+          <meta property="fc:frame:button:1:target" content="${data[dataId-1].url}" />
+          <meta property="fc:frame:button:2" content="Next" />
+          <title>GivitNFT Communities</title>
         </head>
+        <body>
+          <h1>GivitNFT. Visit us in <a href="https://givitnft.com">https://givitnft.com</a></h1>
+        </body>
       </html>
-    `)
+    `);
+  } catch (error) {
+    console.error(error)
+    return c.json({ error: 'Invalid request' }, 400)
+  }
+})
+
+app.post('/', async (c) => {
+  try {
+    console.log('entro en el post');
+    const body = await c.req.json<FrameSignaturePacket>();
+    console.log(body);
+
+    const framePostUrl = c.req.url;
+    console.log(framePostUrl);
+  // <meta property="fc:frame:post_url" content="${framePostUrl}" />
+
+    const { buttonIndex } = body.untrustedData;
+
+    return c.html(html`
+      <html lang="en">
+        <head>
+          <meta property="og:image" content="${data[buttonIndex-1].image}" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="${data[buttonIndex-1].image}" />
+          <meta property="fc:frame:button:1" content="Go to ${data[buttonIndex-1].url}" />
+          <meta property="fc:frame:button:1:action" content="link" />
+          <meta property="fc:frame:button:1:target" content="${data[buttonIndex-1].url}" />
+          ${buttonIndex > 1 ? `<meta property="fc:frame:button:${buttonIndex-1}" content="Previous" />` : ''}
+          ${buttonIndex < data.length ? `<meta property="fc:frame:button:${buttonIndex+1}" content="Next" />` : ''}
+          <title>GivitNFT Communities</title>
+        </head>
+        <body>
+          <h1>GivitNFT. Visit us in <a href="https://givitnft.com">https://givitnft.com</a></h1>
+        </body>
+      </html>
+    `);
   } catch (error) {
     console.error(error)
     return c.json({ error: 'Invalid request' }, 400)
